1e37c3505a3ff275268a753e916ea1c1f15329b3
Marinko Peso <marinko.peso@gmail.com>
2018-11-24 20:53:08 +0100

    basic cli setup :beer:

---
+++ cli.js
@@ -0,0 +1,29 @@
+#!/usr/bin/env node
+'use strict';
+
+const meow = require('meow');
+
+
+const meowText = `
+Usage:
+  $ eurojackpot <options>
+
+Options:
+  --numbers, -n  Define your numbers to check against (last 2 are extras)
+
+Examples:
+  $ eurojackpot
+  $ eurojackpot --numbers 12,15,20,30,34,4,5
+`;
+
+const meowFlags = {
+  numbers: {
+    type: 'string',
+    alias: 'n',
+    default: ''
+  }
+};
+
+const cli = meow(meowText, meowFlags);
+console.log(cli.input);
+console.log(cli.flags);
