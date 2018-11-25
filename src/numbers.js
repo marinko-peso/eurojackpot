df885f1c468c3c6960f5d7fcccb666e996deaf80
Marinko Peso <marinko.peso@gmail.com>
2018-11-25 18:05:26 +0100

    initial api approach

--- src/numbers.js
+++ src/numbers.js
@@ -1,16 +1,21 @@
 const cheerio = require('cheerio');
 const rp = require('request-promise');
-const config = require('../package.json');
+const appConfig = require('../package.json');
 
 
-function getNumbers() {
-  const numbers = [];
+/**
+ * Get resutls by scrapping them from the webpage.
+ * Secondary method in case api is not working.
+ */
+function getNumbersFromScrapping() {
   return rp({
     method: 'GET',
-    url: config.resultsUrl
+    url: appConfig.config.scrapUrl
   })
   .then(htmlString => {
     const $ = cheerio.load(htmlString);
+    // TODO: change to map.
+    const numbers = [];
     $('#results').first().find('li').each((index, element) => {
       numbers.push(
         parseInt($(element).text())
@@ -24,4 +29,28 @@ function getNumbers() {
 }
 
 
-module.exports = { getNumbers };
+/**
+ * Get results of the last draw using an api.
+ * Only extract winning numbers.
+ */
+function getNumbersFromApi() {
+  return rp({
+    method: 'GET',
+    url: appConfig.config.apiUrl
+  })
+  .then(d => {
+    const data = JSON.parse(d).last;
+    const numbers = [];
+    numbers.push(...data.numbers);
+    numbers.push(...data.euroNumbers);
+    return numbers;
+  })
+  .catch(err => {
+    return [];
+  });
+}
+
+getNumbersFromApi();
+
+
+module.exports = { getNumbersFromScrapping, getNumbersFromApi };
